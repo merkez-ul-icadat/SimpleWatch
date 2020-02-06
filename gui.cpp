d5fb1a3acee7e755343d771f5ddcfd9516a93fec
blangus <burakkelebek@gmail.com>
2020-02-06 21:26:34 +0100

    added gps code from example to menu item

--- gui.cpp
+++ gui.cpp
@@ -1434,14 +1434,57 @@ void gps_sw_event_cb(uint8_t index, bool en)
 }
 static void gps_event_cb()
 {
-  Switch::switch_cfg_t cfg[3] = {{"Switch", gps_sw_event_cb}, {"Scan", gps_sw_event_cb}, {"NTP Sync", gps_sw_event_cb}};
-  sw = new Switch;
-  sw->create(cfg, 3, []() {
-    delete sw;
-    sw = nullptr;
-    menuBars.hidden(false);
-  });
-  sw->align(bar.self(), LV_ALIGN_OUT_BOTTOM_MID);
+
+  TTGOClass *ttgo;
+  TFT_eSPI *tft ;
+  S7XG_Class *s7xg;
+  char buff[256];
+  Serial.begin(115200);
+
+    ttgo = TTGOClass::getWatch();
+    ttgo->begin();
+    ttgo->openBL();
+    //! Create a new pointer to save the display object
+    tft = ttgo->eTFT;
+
+    tft->fillScreen(TFT_BLACK);
+    tft->setTextFont(2);
+    tft->println("Begin ublox M8M/GPS Module...");
+
+    //! Open s7xg power
+    ttgo->enableLDO4();
+    ttgo->enableLDO3();
+
+    ttgo->s7xg_begin();
+    s7xg = ttgo->s7xg;
+
+    int len = 0;
+    int retry = 0;
+    do {
+        len = s7xg->getHardWareModel().length();
+        if (len == 0 && retry++ == 5) {
+            s7xg->reset();
+            retry = 0;
+            Serial.println("Reset s7xg chip");
+        }
+        if (len == 0)
+            delay(1000);
+    } while (len == 0);
+
+    Serial.println("Found s7xg module,Start gps module");
+
+    s7xg->gpsReset();
+    s7xg->gpsSetLevelShift(true);
+    s7xg->gpsSetStart();
+    s7xg->gpsSetSystem(0);
+    s7xg->gpsSetPositioningCycle(1000);
+    s7xg->gpsSetPortUplink(20);
+    s7xg->gpsSetFormatUplink(1);
+    s7xg->gpsSetMode(1);
+    
+    tft->setCursor(0, 0);
+    tft->fillScreen(TFT_BLACK);
+    tft->println("Positioning...");
 }
 /*****************************************************************
             ! Lora EVENT
